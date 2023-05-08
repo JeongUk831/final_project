51dadcb094f93093cf9d23fe390cb1c2092e0a17
sumin895 <iris8085@naver.com>
2023-05-08 16:33:37 +0900

    complited

--- data/mongo.py
+++ data/mongo.py
@@ -1,6 +1,9 @@
 import pymongo
-
+import pandas as pd
 conn = pymongo.MongoClient('localhost', 27017)
 db = conn.get_database('final')
 
 collection = db.get_collection('hospital')
+
+files = pd.read_csv('final_project\data\hospital.csv', encoding='utf-8')
+collection.insert_many(files.to_dict('records'))
